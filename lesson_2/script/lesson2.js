3320c3975be4afe1038f78f0285f9c8d692d99f1
Liana666 <liana.nikitina98@gmail.com>
2022-03-11 21:13:08 +0300

    completed 1 and 2 tasks

--- lesson_2/script/lesson2.js
+++ lesson_2/script/lesson2.js
@@ -1,8 +1,9 @@
+///////////////////////task 1
+
 function makeObjectDeepCopy(obj) {
     const objDeepCopy = {};
 
     for (item in obj) {
-
         if (typeof obj[item] === 'object') {
             objDeepCopy[item] = makeObjectDeepCopy(obj[item]);
             continue;
@@ -13,3 +14,37 @@ function makeObjectDeepCopy(obj) {
 
     return objDeepCopy;
 }
+
+///////////////////////task 2
+
+function isValidNumbers(number) {
+    return typeof number === 'number' && !isNaN(number);
+}
+
+function isValidArray(array) {
+    return Array.isArray(array) && arr.every(item => {
+        return isValidNumbers(item);
+    })
+}
+
+function selectFromInterval(array, startIntervalValue, endIntervalValue) {
+    const isValid = isValidArray(array) && isValidNumbers(startIntervalValue) && isValidNumbers(endIntervalValue);
+    let result = [];
+
+    if (!isValid) {
+        throw new Error('Ошибка! невалидное число в интервале или массиве!');
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        if (startIntervalValue < endIntervalValue) {
+            array[i] >= startIntervalValue && array[i] <= endIntervalValue ? result.push(array[i]) : null;
+        } else {
+            array[i] >= endIntervalValue && array[i] <= startIntervalValue ? result.push(array[i]) : null;
+        }
+    }
+
+    return result.sort((firstValue, secondValue) => {
+        return firstValue - secondValue;
+    });
+}
+
