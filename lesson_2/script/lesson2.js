fa5f310073da297ff299050ecbcc62d9b22ebb90
Liana666 <liana.nikitina98@gmail.com>
2022-03-12 12:41:55 +0300

    completed 3 task and add comments

--- lesson_2/script/lesson2.js
+++ lesson_2/script/lesson2.js
@@ -1,9 +1,32 @@
-///////////////////////task 1
+/////////////////////// Helper Functions
+
+///// Check a value for validity (value is number, no NaN and no Infinity)
+function isValidNumbers(number) {
+    return typeof number === 'number' && Number.isFinite(number);
+}
+
+///// Check a array for validity (is array, every value is number, no NaN and no Infinity)
+function isValidArray(array) {
+    return Array.isArray(array) && array.every((item) => {
+        return isValidNumbers(item);
+    });
+}
+
+///// If value not valid throw Error
+function checkedValueIsValid(isValid) {
+    if (!isValid) {
+        throw new Error('Ошибка! невалидное число!');
+    }
+}
+
+
+//////////////////////// task 1
 
 function makeObjectDeepCopy(obj) {
     const objDeepCopy = {};
 
     for (item in obj) {
+
         if (typeof obj[item] === 'object') {
             objDeepCopy[item] = makeObjectDeepCopy(obj[item]);
             continue;
@@ -15,32 +38,22 @@ function makeObjectDeepCopy(obj) {
     return objDeepCopy;
 }
 
-///////////////////////task 2
-
-function isValidNumbers(number) {
-    return typeof number === 'number' && !isNaN(number);
-}
-
-function isValidArray(array) {
-    return Array.isArray(array) && arr.every(item => {
-        return isValidNumbers(item);
-    })
-}
+/////////////////////// task 2
 
 function selectFromInterval(array, startIntervalValue, endIntervalValue) {
     const isValid = isValidArray(array) && isValidNumbers(startIntervalValue) && isValidNumbers(endIntervalValue);
     let result = [];
 
-    if (!isValid) {
-        throw new Error('Ошибка! невалидное число в интервале или массиве!');
-    }
+    checkedValueIsValid(isValid);
 
     for (let i = 0; i < array.length; i++) {
+
         if (startIntervalValue < endIntervalValue) {
             array[i] >= startIntervalValue && array[i] <= endIntervalValue ? result.push(array[i]) : null;
         } else {
             array[i] >= endIntervalValue && array[i] <= startIntervalValue ? result.push(array[i]) : null;
         }
+
     }
 
     return result.sort((firstValue, secondValue) => {
@@ -48,3 +61,31 @@ function selectFromInterval(array, startIntervalValue, endIntervalValue) {
     });
 }
 
+
+///////////////////////////// task 3
+
+const myIterable = { from: 1, to: 4 };
+
+myIterable[Symbol.iterator] = function () {
+    let current = this.from;
+    let last = this.to;
+    const isValid = isValidNumbers(current) && isValidNumbers(last) && myIterable.from < myIterable.to;
+
+    return {
+        next() {
+
+            checkedValueIsValid(isValid);
+
+            if (current <= last) {
+                return {
+                    done: false,
+                    value: current++
+                };
+            } else {
+                return {
+                    done: true
+                };
+            }
+        }
+    };
+};
