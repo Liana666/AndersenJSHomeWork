a254f37e3c039dfd6522568a9d1702f2e6da8461
Liana <liana.nikitina98@gmail.com>
2022-03-30 17:55:26 +0300

    completed all operation, left to refactor

--- script/script.js
+++ script/script.js
@@ -80,30 +80,73 @@ class Calculator {
         this.#previosOperand = '';
     }
 
+    factorial(number) {
+        return number !== 1 ? number * this.factorial(number - 1) : 1;
+    }
 
-    setPlusMinus() {
-        if (this.#currenOperand !== '') {
-            this.#currenOperand = -this.#currenOperand;
-        } else {
-            this.#previosOperand = -this.#previosOperand;
+
+    changeNumber(operation) {
+
+        console.log(operation)
+        if (this.#currenOperand === '') {
+            return;
+        }
+
+        switch (operation) {
+            case '±':
+
+                if (this.#currenOperand !== '') {
+                    this.#currenOperand = -this.#currenOperand;
+                } else {
+                    this.#previosOperand = -this.#previosOperand;
+                }
+
+                break;
+
+
+            case '1/x':
+
+                if (this.#currenOperand !== '') {
+                    this.#currenOperand = 1 / this.#currenOperand;
+                } else {
+                    this.#previosOperand = 1 / this.#previosOperand;
+                }
+
+                break;
+
+
+            case '√x':
+
+                if (this.#currenOperand !== '') {
+                    this.#currenOperand = Math.sqrt(this.#currenOperand);
+                } else {
+                    this.#previosOperand = Math.sqrt(this.#previosOperand);
+                }
+
+                break;
+
+
+            case 'x!':
+
+                if (this.#currenOperand !== '') {
+                    this.#currenOperand = this.factorial(this.#currenOperand);
+                } else {
+                    this.#previosOperand = this.factorial(this.#previosOperand);
+                }
+
+                break;
+
+
+            default:
+                return;
         }
     }
 
-    bracketeOpearation(bracket) {
-        console.log(bracket)
-        if (bracket === '(') {
-            this.#currenOperand = '(' + this.#currenOperand
-        } else {
-            this.#currenOperand = ')' + this.#currenOperand
-        }
-    }
 
 
     trigonometricCompute(operation) {
         let operationResult;
-        console.log(this.#currenOperand)
         const currentNumber = parseFloat(this.#currenOperand);
-        console.log(currentNumber)
 
         this.#operation = operation;
 
@@ -181,55 +224,70 @@ const calculator = new Calculator(currentView);
 
 
 keyboard.addEventListener('click', function onclickKey(e) {
+    let targetElement = e.target;
+    const targetElementText = e.target.innerText;
 
-    if (e.target.hasAttribute('data-number')) {
-        calculator.appendNumber(e.target.innerText);
+
+    if (targetElement.hasAttribute('data-number')) {
+        calculator.appendNumber(targetElementText);
         calculator.updateView();
     }
 
-    if (e.target.hasAttribute('data-delete')) {
+    if (targetElement.hasAttribute('data-delete')) {
         calculator.delete();
         calculator.updateView();
     }
 
-    if (e.target.hasAttribute('data-operation')) {
-        calculator.changeOperation(e.target.innerText);
+    if (targetElement.hasAttribute('data-operation')) {
+        calculator.changeOperation(targetElementText);
         calculator.updateView();
     }
 
-    if (e.target.hasAttribute('data-equal')) {
+    if (targetElement.hasAttribute('data-equal')) {
         calculator.mathematicalСompute();
         calculator.updateView();
         calculator.clear();
     }
 
-    if (e.target.hasAttribute('data-all-clear')) {
+    if (targetElement.hasAttribute('data-all-clear')) {
         calculator.clear();
         calculator.updateView();
     }
 
-    if (e.target.hasAttribute('data-scale')) {
+    if (targetElement.hasAttribute('data-scale')) {
         calculatorWrapper.classList.toggle('scale');
     }
 
-    if (e.target.hasAttribute('data-deg-or-rad')) {
-        calculator.setDegOrRad(e.target.innerText);
+    if (targetElement.hasAttribute('data-deg-or-rad')) {
+        calculator.setDegOrRad(targetElementText);
         e.target.innerText = calculator.getDegOrRad();
     }
 
-    if (e.target.hasAttribute('data-trigonometric')) {
-        calculator.trigonometricCompute(e.target.innerText);
+    if (targetElement.hasAttribute('data-trigonometric')) {
+        calculator.trigonometricCompute(targetElementText);
         calculator.updateView();
     }
 
-    if (e.target.hasAttribute('data-plus-minus')) {
+    if (targetElement.hasAttribute('data-plus-minus')) {
         calculator.setPlusMinus();
         calculator.updateView();
     }
 
-    if (e.target.hasAttribute('data-bracket')) {
-        calculator.bracketeOpearation();
+    if (targetElement.hasAttribute('data-constant')) {
+        let constant;
+        targetElementText === '𝜋' ? constant = Math.PI.toFixed(8) : constant = Math.E.toFixed(8)
+
+        calculator.appendNumber(constant);
         calculator.updateView();
     }
 
+    if (targetElement.hasAttribute('data-change-number')) {
+        calculator.changeNumber(targetElementText);
+        calculator.updateView();
+    }
+
+
 })
+
+
+//console.log()
